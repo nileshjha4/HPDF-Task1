48de346d32a85f91f5ff8fdf4be58d219bfb4d2c
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-12-23 20:59:13 +0530

    Removed redundant routes

--- app.py
+++ app.py
@@ -7,18 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/index/')
-def index():
-    return 'Index page'
-    
-@app.route('/user/<username>/')
-def show_user_profile(username):
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>/')
-def show_post(post_id):
-    return 'Post %d' % post_id
-
 @app.route('/authors/')
 def getAuthorsCount():
     data = requests.get('https://jsonplaceholder.typicode.com/users').json()
