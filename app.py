3d6660369905a3a47efeb053a27137921536c4dc
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-12-25 00:04:27 +0530

    Added two routes to set  as well as read cookies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 import requests
 
 app = Flask(__name__)
@@ -15,6 +15,20 @@ def getAuthorsCount():
     for i in posts:
        count[i['userId']] += 1
     return render_template('authors.html', data = data, count = count)
+
+@app.route('/setcookie/', methods=['GET','POST'])
+def setCookie():
+    if request.method == 'POST' :
+        resp = make_response("Cookies set sucessfully")
+        resp.set_cookie('name', request.form.get('name') )
+        resp.set_cookie('age', request.form.get('age') )
+        return resp
+    elif request.method == 'GET' :
+        return render_template('setcookie.html')
+
+@app.route('/getcookie/')
+def getCookie():
+    return render_template('getcookie.html', name=request.cookies.get('name'), age=request.cookies.get('age') )
 
 if __name__ == '__main__':
     app.run(debug=True)
